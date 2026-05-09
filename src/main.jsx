141404a1115c41e65b676175246b4b2121c8fb68
kitkatkutkit <lythanhtam2102@gmail.com>
2026-05-10 02:48:49 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Kiểm tra xem element có tồn tại không trước khi render
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.error("Không tìm thấy thẻ div với id là 'root' trong file index.html bạn ơi!");
+}
